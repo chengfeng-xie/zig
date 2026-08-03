4692a91a5574f7fb6ddd62bb93d0f1f4b17fd11f
Alex Rønne Petersen <alex@alexrp.com>
2026-08-03 21:28:11 +0200

    std.zig.system.x86: add avx512bmm detection

--- lib/std/zig/system/x86.zig
+++ lib/std/zig/system/x86.zig
@@ -357,9 +357,11 @@ fn detectNativeFeatures(cpu: *Target.Cpu) void {
 
         // AMD uses a different bit for prefetchi.
         setFeature(cpu, .prefetchi, bit(leaf.eax, 20));
+        setFeature(cpu, .avx512bmm, bit(leaf.eax, 23) and has_avx512_save);
     } else {
         for ([_]Target.x86.Feature{
             .prefetchi,
+            .avx512bmm,
         }) |feat| {
             setFeature(cpu, feat, false);
         }

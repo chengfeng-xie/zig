5dbad8c51e6c16a02c5042545154b193aafa9007
Alex Rønne Petersen <alex@alexrp.com>
2026-08-04 12:16:20 +0200

    compiler: add loongarch(32,64)_preserve_none calling convention support

--- lib/std/Target.zig
+++ lib/std/Target.zig
@@ -1955,9 +1955,11 @@ pub const Cpu = struct {
                 => &.{.lanai},
 
                 .loongarch64_lp64,
+                .loongarch64_preserve_none,
                 => &.{.loongarch64},
 
                 .loongarch32_ilp32,
+                .loongarch32_preserve_none,
                 => &.{.loongarch32},
 
                 .m68k_sysv,
